5b2225eb29a1354b339053d175c98742d605d20a
Julio Forni <jcforni@gmail.com>
2023-01-10 11:24:21 -0300

    Clase 40

--- 02-react-app/src/App.jsx
+++ 02-react-app/src/App.jsx
@@ -2,20 +2,27 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  const nombre = "Julio"
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      
+      <header className='App-header'>
+        <div>
+          <a href="https://vitejs.dev" target="_blank">
+            <img src="/vite.svg" className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://reactjs.org" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <p>
+          <button> - </button>
+          <h2>10</h2>
+          <button> + </button>
+        </p>
+
+        <h1>{new Date().getDate() + '/' + new Date().getMonth()+1 + '/' + new Date().getFullYear() }</h1>
+      </header>      
     </div>
   )
 }
